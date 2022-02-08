17ef1d457853f17d5161d1dae49d3aac58c74c2f
Amirhossein Khodaei <amirhosseinkhodaei@Amirhosseins-MacBook-Pro.local>
2022-02-08 10:02:38 +0330

    fix: update skills.

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,12 +1,17 @@
 export const skills = [
   "React",
   "Redux",
+  "Hooks",
   "Laravel",
-  "JS, TS",
+  "JS",
+  "HTML/CSS",
   "Sass",
+  "Responsive Design",
+  "axios",
   "Wordpress",
   "GitHub/GitLab CI/CD",
   "Antd",
+  "Material UI",
   "Bootstrap",
   "MySQL Database Managment",
 ];
